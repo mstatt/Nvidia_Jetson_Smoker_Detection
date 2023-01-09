2e47ff0f3b64b061ada19b00a0ae6b52de0e9206
Michael Stattelman <mstatt@users.noreply.github.com>
2023-01-09 18:57:59 +0400

    Update insatlls.sh

--- insatlls.sh
+++ insatlls.sh
@@ -22,6 +22,10 @@ sudo apt-get install -y libhdf5-dev protobuf-compiler python3-venv
 sudo apt-get install -y libprotobuf-dev libgoogle-glog-dev libgflags-dev
 sudo apt-get install -y libcanberra-gtk-module
 sudo apt-get install -y v4l-utils
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+sudo apt-get update 
+sudo apt-get install -y sublime-tex
 
 
 sudo -H pip3 install -U jetson-stats
