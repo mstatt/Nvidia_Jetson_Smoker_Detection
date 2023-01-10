65dcf9ca877086f72498117b238c5cf5b8f1360c
Michael Stattelman <mstatt@users.noreply.github.com>
2023-01-10 06:38:01 +0400

    Updates

--- max.sh
+++ max.sh
@@ -2,6 +2,7 @@
 # Script to kick off Max performance on Nano
 
 sudo jetson_clocks
+# Kisk of fan to 1/2 speed
 sudo sh -c 'echo 128 > /sys/devices/pwm-fan/target_pwm'
 # Uncomment below if you want to start the jtop system monitor
 #jtop
